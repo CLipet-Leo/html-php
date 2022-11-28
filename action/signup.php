7a73c6029946eeb25cfbd12888e105d49c496c2d
Clipet-Leo <leoclipet54@gmail.com>
2022-11-28 17:28:21 +0100

    initialize repository

--- action/signup.php
+++ action/signup.php
@@ -1,11 +1,18 @@
 <?php
-$sql = "INSERT INTO user(email,password) VALUES(:email,:password)";
+$sql = "SELECT * FROM user WHERE username=:username AND password=SHA1(:password)";
 $dataBinded=array(
-    ':email'   => $_POST['email'],
+    ':username'   => $_POST['username'],
     ':password'=> $_POST['password']
 );
 $pre = $pdo->prepare($sql);
 $pre->execute($dataBinded);
+$user = $pre->fetch(PDO::FETCH_ASSOC);
+if(empty($user)){ //vérifie si le resultat est vide !
+     //non connecté
+     echo "Utilisateur ou mot de passe incorrect !";
+}else{
+     $_SESSION['user'] = $user; //on enregistre que l'utilisateur est connecté
+}
 
 header('Location: ../index.php')
 
