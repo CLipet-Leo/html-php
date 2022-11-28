266276118b92a769bb8c5818177d8352e1563ef0
Clipet-Leo <leoclipet54@gmail.com>
2022-11-28 17:47:08 +0100

    initialize repository

--- index.php
+++ index.php
@@ -4,19 +4,19 @@
 <head>
 </head>
 <body>
-   <?php require "menu.php"; ?>
-   <h1>Liste des utilisateurs</h1>
-   <?php
-   $sql = "SELECT * FROM user"; 
-   $pre = $pdo->prepare($sql); 
-   $pre->execute();
-   $data = $pre->fetchAll(PDO::FETCH_ASSOC);
+    <?php require "menu.php"; ?>
+    <h1>Liste des utilisateurs</h1>
+    <?php
+    $sql = "SELECT * FROM user"; 
+    $pre = $pdo->prepare($sql); 
+    $pre->execute();
+    $data = $pre->fetchAll(PDO::FETCH_ASSOC);
    
-   foreach($data as $user){ ?>
-   <div class="bloc_user">
+    foreach($data as $user){ ?>
+    <div class="bloc_user">
         <h2><?php echo $user['first_name']." ".$user['last_name'] ?></h2>
         <span class="email"><?php echo $user['email'] ?></span>
-   </div>
-   <?php } ?>
+    </div>
+    <?php } ?>
 </body>
 </html>
