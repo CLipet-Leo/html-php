7048579a3b24c6c879b9e0cc5db1234df37e674f
Clipet-Leo <leoclipet54@gmail.com>
2022-11-30 09:58:25 +0100

    php fix

--- action/admin.php
+++ action/admin.php
@@ -6,14 +6,4 @@ if(isset($_SESSION['user']) AND $_SESSION['user']['admin']==1){
     echo "Bonjour, administrateur ".$_SESSION['user']['login'];
 
 }
-
-require "project.php";
-foreach ($project as $project);
-<div>
-<h2><?php echo $project['id'] ?></h2>
-<form method="post" action="action/delate_project.php"
-</div>
-
-
-
 ?>
